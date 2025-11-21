1da6b3ba470c9eeefd2770f1d190e2a488b9b8bb
Mohit Sharma <imohito@amazon.com>
2025-11-21 16:35:56 +0530

    Index file start for aws lambda

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 require("dotenv").config();
 
-// Starts tiny HTTP server (required by AWS App Runner)
-const { startHttpServer } = require("./healthcheck");
-startHttpServer();
-
 const app = require("./app");
+const { startHttpServer } = require("./http/healthcheck");
 
 (async () => {
   await app.start();
   console.log("⚡ Slack forwarder running via Socket Mode");
+
+  startHttpServer();
 })();
