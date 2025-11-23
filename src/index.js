b87179529c38067f78a4df48b2c45425faa0153c
Mohit Sharma <imohito@amazon.com>
2025-11-23 17:35:06 +0530

    Lightweight index.js for init

--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
 require("dotenv").config();
 
-const express = require("express");
-const { app, receiver } = require("./app");
+const { app } = require("./app");
 const { startHttpServer } = require("./http/healthcheck");
-
-// Slack URL verification MUST NOT parse body before Bolt sees it
-receiver.router.post("/slack/events", (req, res) => {
-  if (req.body?.type === "url_verification") {
-    return res.send(req.body.challenge);
-  }
-  return res.sendStatus(200);
-});
 
 (async () => {
   const port = process.env.PORT || 3000;
-  await app.start(port);
 
+  await app.start(port);
   console.log(`⚡ Slack forwarder running in HTTP mode on port ${port}`);
 
   startHttpServer();
