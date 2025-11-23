0b9c4f7757b88a27c75e497c8bd50adb6c1666c8
Mohit Sharma <imohito@amazon.com>
2025-11-23 17:28:10 +0530

    Parser adjustment for slack app

--- src/index.js
+++ src/index.js
@@ -4,16 +4,12 @@ const express = require("express");
 const { app, receiver } = require("./app");
 const { startHttpServer } = require("./http/healthcheck");
 
-// Required so Slack's JSON body is parsed
-receiver.app.use(express.json());
-
-// URL Verification handler
-receiver.app.post("/slack/events", (req, res) => {
+// Slack URL verification MUST NOT parse body before Bolt sees it
+receiver.router.post("/slack/events", (req, res) => {
   if (req.body?.type === "url_verification") {
-    return res.status(200).send(req.body.challenge);
+    return res.send(req.body.challenge);
   }
-  // Bolt will handle the rest
-  res.sendStatus(200);
+  return res.sendStatus(200);
 });
 
 (async () => {
